041638edd9cc98204748610c878eb8938919d4d9
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2021-09-07 00:16:10 +0200

    apdate api base url

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -3,8 +3,8 @@ import { IAppState } from '../store/ducks/rootReducer';
 
 const axiosApiInstance = axios.create();
 
-// const API_BASE_URL = 'https://mcheref-portfolio-api.herokuapp.com/api/';
-const API_BASE_URL = 'http://localhost:8080/api/';
+const API_BASE_URL = 'https://mcheref-portfolio-api.herokuapp.com/api/';
+//const API_BASE_URL = 'http://localhost:8080/api/';
 
 export const getPortfolioData = axiosApiInstance.get<IAppState>(
   API_BASE_URL + 'portfolio'
