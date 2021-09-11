46532e866cb92c3fc9335bafd5d4778c282bff85
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2021-09-11 13:52:57 +0200

    add env vars

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -3,7 +3,7 @@ import { IportfolioState } from '../store/ducks/rootReducer';
 
 const axiosApiInstance = axios.create();
 
-const API_BASE_URL = 'https://mcheref-portfolio-api.herokuapp.com/api/';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 //const API_BASE_URL = 'http://localhost:8080/api/';
 
 export const getPortfolioData = axiosApiInstance.get<IportfolioState>(
