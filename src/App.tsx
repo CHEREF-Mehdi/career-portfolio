ea16bd1bd508339d93d8654ba01b4dc8ccb7f811
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2021-09-11 19:00:27 +0200

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import Container from './components/Container';
 import ReactGa from 'react-ga';
 
 const App: React.FC = () => {
-  React.useEffect(() => {    
+
+  React.useEffect(() => {
     const gaTrackingId: string = process.env.REACT_APP_GA_TRAKING_ID || '';
     console.log(gaTrackingId);
     ReactGa.initialize(gaTrackingId);
     ReactGa.pageview('/');
-  }, []);
+  }, [process.env.REACT_APP_GA_TRAKING_ID]);
+
   return (
     <Provider store={store}>
       <ToastContainer />
