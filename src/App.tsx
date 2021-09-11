4f056ef9425e4f8427489ab06056b47d279adac3
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2021-09-11 18:55:24 +0200

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import store from './store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Container from './components/Container';
+import ReactGa from 'react-ga';
 
 const App: React.FC = () => {
+  React.useEffect(() => {    
+    const gaTrackingId: string = process.env.REACT_APP_GA_TRAKING_ID || '';
+    console.log(gaTrackingId);
+    ReactGa.initialize(gaTrackingId);
+    ReactGa.pageview('/');
+  }, []);
   return (
     <Provider store={store}>
-      <ToastContainer/>
-      <Container/>
+      <ToastContainer />
+      <Container />
     </Provider>
   );
 };
--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -4,7 +4,7 @@ import { IportfolioState } from '../store/ducks/rootReducer';
 const axiosApiInstance = axios.create();
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
-console.log({API_BASE_URL});
+console.log(process.env.GA_TRAKING_ID);
 
 export const getPortfolioData = axiosApiInstance.get<IportfolioState>(
   API_BASE_URL + 'portfolio'
