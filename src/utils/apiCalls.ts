f04a02c62d5628a220510d394701a569b66af775
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2022-11-27 22:24:02 +0100

    update url api

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -5,23 +5,29 @@ import { ICareerData } from '../store/utils/dataTypes';
 const axiosApiInstance = axios.create();
 
 // Add a request interceptor
-axiosApiInstance.interceptors.response.use(function (config) {
-  // Do something before request is sent
-  return config;
-}, function (error) {  
-  if (error.message === 'Network Error') {     
-    toastNotify(
-      'Network Error! Please check your internet connection',
-      'error'
-    );
+axiosApiInstance.interceptors.response.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    if (error.message === 'Network Error') {
+      toastNotify(
+        'Network Error! Please check your internet connection',
+        'error'
+      );
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const getPortfolioData = () =>
-  axiosApiInstance.get<ICareerData>(API_BASE_URL + 'portfolio-data');
+  axiosApiInstance.get<ICareerData>(API_BASE_URL + 'portfolio', {
+    headers: { 'api-key': API_KEY },
+  });
 
 export const contactMe = (data: IContactFormData) =>
   axiosApiInstance.post(API_BASE_URL + 'contact', data);
